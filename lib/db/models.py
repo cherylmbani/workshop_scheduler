2af331cf946e81ac45f816cb4d7e6afec0397619
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-08-26 08:04:07 +0300

    Creating the Base class, organizer and establishing its 1:M relationship with workshops

--- lib/db/models.py
+++ lib/db/models.py
@@ -3,3 +3,15 @@ from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 
 #Connect classes to the database using Base
 Base = declarative_base 
+
+class Organizer(Base):
+    __tablename__ = "organizers"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(40), nullable = False)
+    last_name = Column(String(40), nullable = False)
+    email_address = Column(String)
+    phone_number = Column(Integer, nullable = False)
+    workshops = relationship("Workshop", back_populates="organizer")
+
+
+
